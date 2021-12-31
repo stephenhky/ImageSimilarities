8568e43975d2a1b151a92a10e188de7bc5f62681
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2021-12-31 17:13:12 -0500

    further modularization

--- imgsim.py
+++ imgsim.py
@@ -4,6 +4,9 @@ from scipy.spatial.distance import euclidean, cosine
 import cv2
 
 
+# Reference: https://www.geeksforgeeks.org/measure-similarity-between-images-using-python-opencv/
+
+
 # convert PIL image to OpenCV image: https://stackoverflow.com/questions/14134892/convert-image-from-pil-to-opencv-format
 def convert_PIL_to_opencv(pilimg):
     npimg = np.array(pilimg.convert('RGB'))
@@ -11,20 +14,30 @@ def convert_PIL_to_opencv(pilimg):
     return cvimg
 
 
-# Reference: https://www.geeksforgeeks.org/measure-similarity-between-images-using-python-opencv/
-def histdistance(img1, img2, normalize=True):
+def compute_grayscale_histogram(cvimg, normalize=False):
+    grayimg = cv2.cvtColor(cvimg, cv2.COLOR_BGR2GRAY)
+    hist = cv2.calcHist([grayimg], [0], None, [256], [0, 256])
+    if normalize:
+        return hist / np.linalg.norm(hist)
+    else:
+        return hist
+
+
+def imagepair_distance(img1, img2, normalize=False):
     cvimg1 = convert_PIL_to_opencv(img1)
     cvimg2 = convert_PIL_to_opencv(img2)
 
-    grayimg1 = cv2.cvtColor(cvimg1, cv2.COLOR_BGR2GRAY)
-    grayimg2 = cv2.cvtColor(cvimg2, cv2.COLOR_BGR2GRAY)
-
-    hist1 = cv2.calcHist([grayimg1], [0], None, [256], [0, 256])
-    hist2 = cv2.calcHist([grayimg2], [0], None, [256], [0, 256])
-
-    # normalize
-    if normalize:
-        hist1 /= np.linalg.norm(hist1)
-        hist2 /= np.linalg.norm(hist2)
+    hist1 = compute_grayscale_histogram(cvimg1, normalize=normalize)
+    hist2 = compute_grayscale_histogram(cvimg2, normalize=normalize)
 
     return euclidean(hist1, hist2)
+
+
+def imagepair_similarity(img1, img2):
+    cvimg1 = convert_PIL_to_opencv(img1)
+    cvimg2 = convert_PIL_to_opencv(img2)
+
+    hist1 = compute_grayscale_histogram(cvimg1)
+    hist2 = compute_grayscale_histogram(cvimg2)
+
+    return 1 - cosine(hist1, hist2)
