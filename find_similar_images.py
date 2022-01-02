f545531921cdb3b3556cc9e1adbffc0ace2da15e
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2022-01-02 11:20:08 -0500

    limit the number of displayed results

--- find_similar_images.py
+++ find_similar_images.py
@@ -14,6 +14,7 @@ def get_argparser():
     argparser.add_argument('--euclidean', default=False, action='store_true', help='Use Euclidean distance. (Default: using cosine similarity)')
     argparser.add_argument('image1path', help='path of the reference image')
     argparser.add_argument('imagepaths', nargs='+', help='paths of images to compare')
+    argparser.add_argument('--n', default=10, type=int, help='number of files shown. (Default: 10)')
     return argparser
 
 
@@ -22,6 +23,7 @@ if __name__ == '__main__':
     eudist = args.euclidean
     img1 = Image.open(args.image1path)
     imagepaths = args.imagepaths
+    n = args.n
 
     resultdict = {}
     for imagepath in tqdm(imagepaths):
@@ -32,7 +34,7 @@ if __name__ == '__main__':
             value = imagepair_similarity(img1, img)
         resultdict[imagepath] = value
 
-    for imgpath, value in sorted(resultdict.items(), key=itemgetter(1), reverse=eudist):
+    for imgpath, value in sorted(resultdict.items(), key=itemgetter(1), reverse=eudist)[-n:]:
         basefilename = os.path.basename(imgpath)
         print('{}: {}'.format(basefilename, value))
 
